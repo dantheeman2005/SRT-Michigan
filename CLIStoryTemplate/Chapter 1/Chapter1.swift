6f65e8a6684fc3339e6bcf1162af62c35c46193d
takeira <takeirabridges@gamil.com>
2023-10-18 16:51:36 -0400

    Initial Commit

--- CLIStoryTemplate/Chapter 1/Chapter1.swift
+++ CLIStoryTemplate/Chapter 1/Chapter1.swift
@@ -6,5 +6,8 @@
 import Foundation
 
 func chapterOne() {
+    print("Once upon a time there was a haunted car that only came alive on Halloween.")
+    print("Everyday it sat in its owner garage waiting to prey on its next victim.")
+    print("Then one day it was so hot the paint melted.... ")
     // Your portion of the story goes here
 }
