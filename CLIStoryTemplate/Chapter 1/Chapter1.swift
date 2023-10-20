ad4160d3eafb39cb224cce98b49449b897144ae5
takeira <takeirabridges@gamil.com>
2023-10-19 23:23:27 -0400

    C1 updated

--- CLIStoryTemplate/Chapter 1/Chapter1.swift
+++ CLIStoryTemplate/Chapter 1/Chapter1.swift
@@ -5,9 +5,19 @@
 
 import Foundation
 
-func chapterOne() {
-    print("Once upon a time there was a haunted car that only came alive on Halloween.")
-    print("Everyday it sat in its owner garage waiting to prey on its next victim.")
-    print("Then one day it was so hot the paint melted.... ")
-    // Your portion of the story goes here
+
+
+var characterName = String ("Azure")
+
+var sentence1 = String ("Once upon a time, deep in the trenches of Detroit, there was a haunted yet beautiful car named \(characterName).")
+var sentence2 = String ("For 364 days it operated as a normal vehicle, but on Halloween the evil that had been festering for nearly a year was ready to be recognized.")
+var sentence3 = String ("Until one night, Devils Night to be exact, a fire broke out next door. Causing the paint on \(characterName) to melt.")
+var sentence4 = String ("Because of that \(characterName)'s shiny silver insides had been burned and bruised with soot, causing the evil to awaken a day early. ")
+var sentence5 = String ("Because of that \(characterName) felt ugly, useless, and bare which send it into a horror spree.")
+
+//var sentence 6: String ("Within hours hundreds of reports of hit and runs, drunk drivers, and "teeneages fooling around had been reported".")
+
+func chapterOne()
+{
+    print(sentence1, sentence2, sentence3, sentence4, sentence5)
 }
