ee40b407f05dcc30176ce534534264e383baf904
takeira <takeirabridges@gamil.com>
2023-10-19 23:23:27 -0400

    C1 updated

--- CLIStoryTemplate/Chapter 1/Chapter1.swift
+++ CLIStoryTemplate/Chapter 1/Chapter1.swift
@@ -5,9 +5,33 @@
 
 import Foundation
 
+var carAwaken = String ("horror and harm was soon to follow")
+
+
+var characterName = String ("Azure")
+
+var sentence1 = String ("Once upon a time, deep in the trenches of Detroit, there was a haunted yet beautiful car named \(characterName).")
+var sentence2 = String ("For 364 days it operated as a normal vehicle, but on Halloween night every year the evil that had been festering for months had the chance to be recognized.")
+var sentence3 = String ("Until one night, Devils Night to be exact, a fire broke out next door, causing the paint on \(characterName) to melt.")
+var sentence4 = String ("Because of that \(characterName)'s one of a kind exterior had been burned and bruised with soot.")
+var sentence5 = String ("The blistering heat from the fire allowed the true evil and wickedness that lied within the car, unbeknownst to \(characterName) and it's owner, to awaken early.")
+var sentence6 = String ("Catching a glimpse of its reflection \(characterName) felt worthless, useless, and bare which meant \(carAwaken).")
+
+var sentence7 = String ("Though \(characterName) was not aesthetically pleasing to look at, it still maintained all its functionallity and more.")
+var sentence8 = String ("For hours \(characterName) terrorized everything and everyone in it's path.")
+var sentence9 = String ("Residents flooded the precinct phone lines with reports of what they believed was a \"drunk driver\" in a \"half burned\" car driving wrecklessly. ")
+
 func chapterOne() {
-    print("Once upon a time there was a haunted car that only came alive on Halloween.")
-    print("Everyday it sat in its owner garage waiting to prey on its next victim.")
-    print("Then one day it was so hot the paint melted.... ")
-    // Your portion of the story goes here
+
+    print(sentence1, sentence2, sentence3, sentence4, sentence5)
+    
+
+    print(sentence6, sentence7, sentence8, sentence9)
 }
+
+
+//. For hours \(characterName) terrorized the local neighborhood.
+
+
+
+
