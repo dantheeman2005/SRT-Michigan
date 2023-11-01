b46b81e1c923005f6a88148551f641fd33d8cc55
takeira <takeirabridges@gamil.com>
2023-11-01 15:09:40 -0400

    c1 final

--- CLIStoryTemplate/Chapter 1/Chapter1.swift
+++ CLIStoryTemplate/Chapter 1/Chapter1.swift
@@ -28,14 +28,32 @@ var sentence7 = String ("Though Azure was not aesthetically pleasing to look at,
 
 var sentence8 = String ("With \(daysUntilNormal) days left before returning back to normal \(characterName)'s main goal was to invoke fear in those who displayed it least.")
 var sentence9 = String ("The burned, dilapitated car would slowly trail behind those who chose to roam the streets at night.")
+var sentence10 = String ("After capturing what seemed to be its fourth victim, a teenage boy, a cold chill ran through the boys body after reading greeting cards that the car had printed for previous victims.")
+var sentence11 = String ("They read the following....")
+
+
+
+
 
 // if they dared to turn around and look at the car, it would flash its lights so bright that it would temporarily blind the victim, if they did not turn
 // around, the car would let them continue their night without a fright
 
+
+//let songsWhenRogue = ["Highway To Hell", "Mr. Sandman", "No Bystanders", "Aktivated","Duck Yo Taco","Sorry Not Sorry"]
+
+let threatsFromRadio = ["Pain", "Mental Manipulation", "Temporary Blindness"]
+
+
+
+// Welcome to Paradise! You are in for a ride! You can expect \(threatsFromRadio) on this journey. So sit back, relax, and see what your future awaits.
+
+
+
+
 func residentsReaction() -> String
 {
     if residentsAngry {
-        return  "Residents flooded the precinct phone lines with reports of what they believed was a \"drunk driver\" in a \"half burned\" car driving wrecklessly. "
+        return  "\(characterName)'s rampage and wreckless driving caused residents to flooded the precinct phone lines with reports of what they believed was a \"drunk driver\" in a \"half burned\" car driving wrecklessly. "
     } else {
         return "It was a regular day."
     }
@@ -49,15 +67,19 @@ func describeEnvironment() -> String
     } else {
         return "With every hour lost \(characterName) moved closer to returning back to normal."
     }
+    
+    
 }
 
-
-
 func chapterOne() {
- 
-    print(sentence1, sentence2, sentence3, sentence4, sentence4, sentence5, sentence7, sentence6)
+    
+    print(sentence1, sentence2, sentence3, sentence4, sentence5, sentence7, sentence6)
     print(residentsReaction())
     print (describeEnvironment())
-    print (sentence8, sentence9)  
+    print (sentence8, sentence9, sentence10,sentence11)
+    
+    for threats in threatsFromRadio
+    { print(" Welcome to Paradise! You are in for a ride! You can expect \(threats) on this journey. So sit back, relax, and see what your future awaits.")
     }
-
+}
+    
