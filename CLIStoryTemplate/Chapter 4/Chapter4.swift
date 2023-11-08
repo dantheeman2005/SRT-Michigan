d6dc157644ec3bc6aaea4ab4edd66329fda3a575
Adebola Fagade <debolafagade90@gmail.com>
2023-11-08 13:13:35 -0500

    Update on ch4

--- CLIStoryTemplate/Chapter 4/Chapter4.swift
+++ CLIStoryTemplate/Chapter 4/Chapter4.swift
@@ -6,5 +6,54 @@
 import Foundation
 
 func chapterFour() {
-    // Your portion of the story goes here
+    enum CarType {
+        case normal
+        case spooky
+    }
+
+    var raceClubMembers = ["Jake", "Sophie", "Carlos", "Lena"]
+    var raceResults: [String: CarType] = [:]
+    let raceTrackLength = 1.5
+    let october = 10
+
+    func raceCar(type: CarType) -> Bool {
+        let isOctober = Int.random(in: 1...12) == october
+        
+        if isOctober && type == .spooky {
+            return true
+        }
+        
+        return !isOctober && type == .normal
+    }
+
+    func race(carType: CarType, racerName: String) {
+        let winner = raceCar(type: carType)
+        raceResults[racerName] = winner ? carType : .normal
+        if winner {
+            print("\(racerName) wins the race with their \(carType == .spooky ? "spooky" : "normal") car!")
+        } else {
+            print("\(racerName) finishes the race but doesn't win this time.")
+        }
+    }
+
+    print("Welcome to the Detroit Street Racing Club!\n")
+
+    for member in raceClubMembers {
+        let carType: CarType = member == "Lena" ? .spooky : .normal
+        race(carType: carType, racerName: member)
+    }
+
+    print("\nRace Results:")
+
+    for (racer, carType) in raceResults {
+        let carDescription = carType == .spooky ? "Spooky" : "Normal"
+        print("\(racer): \(carDescription) Car")
+    }
+
+    let halloweenNight = 31
+    if october == halloweenNight {
+        print("\nIt's Halloween night in Detroit, and the spooky car emerges!")
+        let spookyCarOwner = "Lena"
+        race(carType: .spooky, racerName: spookyCarOwner)
+    }
 }
